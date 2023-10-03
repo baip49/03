1c0a527e98a67b22e74b0732c21de6f6f8cb6934
Baip <40480740+baip49@users.noreply.github.com>
2023-10-02 21:17:43 -0600

    Add files via upload

---
+++ 02/02/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float F = float.Parse(textBox1.Text);
+            float C = (F - 32) * 5.0f / 9.0f;
+            textBox2.Text = C.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            float C = float.Parse(textBox2.Text);
+            float F = (C * 5.0f / 9.0f) + 32;
+            textBox2.Text = C.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0.0";
+            textBox2.Text = "0.0";
+        }
+    }
+}
---
+++ 03.py
@@ -0,0 +1,41 @@
+from tkinter import *
+import tkinter as tk
+app = tk.Tk()
+app.title("Conversor de temperatura") # Cambiar el nombre de la ventana
+
+def reset():
+    entry1.delete(0, tk.END)
+    entry2.delete(0, tk.END)
+def convert_to_fahrenheit():
+    celsius = float(entry2.get())
+    fahrenheit = (celsius * 9 / 5) + 32
+    entry1.delete(0, tk.END)
+    entry1.insert(0, f"{fahrenheit}")
+def convert_to_celsius():
+    fahrenheit = float(entry1.get())
+    celsius = (fahrenheit - 32) * 5 / 9
+    entry2.delete(0, tk.END)
+    entry2.insert(0, f"{celsius}")
+
+label1 = tk.Label(app, text="Fahrenheit:")
+label1.grid(row=0, column=0)
+
+entry1 = tk.Entry(app)
+entry1.grid(row=0, column=1)
+
+button1 = tk.Button(app, text="Convertir a Celcius", command=convert_to_celsius)
+button1.grid(row=0, column=2)
+
+label2 = tk.Label(app, text="Celcius:")
+label2.grid(row=1, column=0)
+
+entry2 = tk.Entry(app)
+entry2.grid(row=1, column=1)
+
+button2 = tk.Button(app, text="Convertir a Fahrenheit", command=convert_to_fahrenheit)
+button2.grid(row=1, column=2)
+
+button3 = tk.Button(app, text="Restablecer", command=reset)
+button3.grid(row=2, column=1)
+
+app.mainloop()
